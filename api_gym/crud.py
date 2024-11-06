614647c07bb7bb406ff023b57a92baf5a798abfb
Riuk <eliandersonm457@gmail.com>
2024-11-06 20:38:02 -0300

    projeto sem token

---
+++ api_gym/crud.py
@@ -0,0 +1,88 @@
+from sqlalchemy.orm import Session
+from .models import Exercicio
+from .schemas import ExercicioCriar
+
+# Função para obter um exercício pelo ID
+def obter_exercicio_por_id(db: Session, exercicio_id: int):
+    return db.query(Exercicio).filter(Exercicio.id == exercicio_id).first()
+
+# Função para criar um exercício
+def criar_exercicio(db: Session, exercicio: ExercicioCriar):
+    db_exercicio = Exercicio(**exercicio.dict())
+    db.add(db_exercicio)
+    db.commit()
+    db.refresh(db_exercicio)
+    return db_exercicio
+
+# Função para criar exercícios 
+def criar_exercicios_iniciais(db: Session):
+    exercicios = [
+
+        {
+            "nome": "Flexão Diamante",
+            "url_video": "https://www.youtube.com/watch?v=PAauHMIhWKg",
+            "descricao": "Flexão de braços diamante é um exercício que envolve as articulações de cotovelo e ombro, solicitando fortemente a musculatura de peitoral, ombro e tríceps. Envolve também a musculatura estabilizadora do quadril e tronco, exercitando indiretamente a musculatura abdominal."
+        },
+        {
+            "nome": "Remada Prancha",
+            "url_video": "https://www.youtube.com/watch?v=LDAetC9WbeM",
+            "descricao": "A remada em prancha com halteres é um bom exercício para intensificar o músculo dorsal e reforçar todos os músculos do core. É um excelente desafio funcional para elevar o nível de intensidade."
+        },
+        {
+            "nome": "Agachamento Com Uma Perna",
+            "url_video": "https://www.youtube.com/watch?v=b-p1hBJ-_tg",
+            "descricao": "O agachamento com uma perna é um dos exercícios mais completos, envolvendo grupos musculares dos membros inferiores e toda a região do core. Quando feito com uma barra, também ativa os paravertebrais."
+        },
+        {
+            "nome": "Agachamento Com Barra",
+            "url_video": "https://www.youtube.com/watch?v=0idszCQ-Ky0",
+            "descricao": "O agachamento livre com barra é um exercício completo da musculação, priorizando quadríceps, glúteos e adutores."
+        },
+        {
+            "nome": "Bíceps Alternados Com Halteres",
+            "url_video": "https://www.youtube.com/watch?v=n-TsjQkOa3c",
+            "descricao": "A rosca alternada é um dos principais exercícios para fortalecimento dos braços, focado nos músculos dos bíceps."
+        },
+        {
+            "nome": "Supino Reto Com Barra",
+            "url_video": "https://www.youtube.com/watch?v=Kr2erpSyu3M",
+            "descricao": "O supino reto com barra permite maior amplitude de movimento, ativando peito, bíceps braquial e tríceps. Requer equilíbrio e coordenação."
+        }
+    
+    ]
+    for exercicio in exercicios:
+        db_exercicio = Exercicio(**exercicio)
+        db.add(db_exercicio)
+    db.commit()
+    return {"mensagem": "Exercícios criados com sucesso"}
+
+# Função get pelo ID
+def obter_exercicio(db: Session, exercicio_id: int):
+    return obter_exercicio_por_id(db, exercicio_id)
+
+# Função get
+def obter_exercicios(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Exercicio).offset(skip).limit(limit).all()
+
+# Função put
+def atualizar_exercicio(db: Session, exercicio_id: int, dados_exercicio: ExercicioCriar):
+    db_exercicio = obter_exercicio_por_id(db, exercicio_id)
+    if db_exercicio:
+        db_exercicio.nome = dados_exercicio.nome
+        db_exercicio.url_video = dados_exercicio.url_video
+        db_exercicio.descricao = dados_exercicio.descricao
+        db.commit()
+        db.refresh(db_exercicio)
+        return db_exercicio
+    return None
+
+# Função delete
+
+
+def deletar_exercicio(db: Session, exercicio_id: int):
+    db_exercicio = obter_exercicio_por_id(db, exercicio_id)
+    if db_exercicio:
+        db.delete(db_exercicio)
+        db.commit()
+        return {"mensagem": "Exercício deletado com sucesso"}
+    return {"mensagem": "Exercício não encontrado"}
---
+++ api_gym/main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+from api_gym.database import SessionLocal, engine
+from api_gym.models import Base
+from api_gym.schemas import ExercicioCriar, ExercicioResposta
+from api_gym.exercicios import (
+    criar_exercicios_iniciais,
+    criar_exercicio,
+    obter_exercicio,
+    obter_exercicios,
+    atualizar_exercicio,
+    deletar_exercicio
+)
+
+# banco de dados
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# sessão do banco de dados
+def obter_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.on_event("startup")
+def inicializacao():
+    db = SessionLocal()
+    exercicios = [
+        
+        {
+            "nome": "Flexão Diamante",
+            "url_video": "https://www.youtube.com/watch?v=PAauHMIhWKg",
+            "descricao": "Flexão de braços diamante é um exercício que envolve as articulações de cotovelo e ombro, solicitando fortemente a musculatura de peitoral, ombro e tríceps. Envolve também a musculatura estabilizadora do quadril e tronco, exercitando indiretamente a musculatura abdominal."
+        },
+        {
+            "nome": "Remada Prancha",
+            "url_video": "https://www.youtube.com/watch?v=LDAetC9WbeM",
+            "descricao": "A remada em prancha com halteres é um bom exercício para intensificar o músculo dorsal e reforçar todos os músculos do core. É um excelente desafio funcional para elevar o nível de intensidade."
+        },
+        {
+            "nome": "Agachamento Com Uma Perna",
+            "url_video": "https://www.youtube.com/watch?v=b-p1hBJ-_tg",
+            "descricao": "O agachamento com uma perna é um dos exercícios mais completos, envolvendo grupos musculares dos membros inferiores e toda a região do core. Quando feito com uma barra, também ativa os paravertebrais."
+        },
+        {
+            "nome": "Agachamento Com Barra",
+            "url_video": "https://www.youtube.com/watch?v=0idszCQ-Ky0",
+            "descricao": "O agachamento livre com barra é um exercício completo da musculação, priorizando quadríceps, glúteos e adutores."
+        },
+        {
+            "nome": "Bíceps Alternados Com Halteres",
+            "url_video": "https://www.youtube.com/watch?v=n-TsjQkOa3c",
+            "descricao": "A rosca alternada é um dos principais exercícios para fortalecimento dos braços, focado nos músculos dos bíceps."
+        },
+        {
+            "nome": "Supino Reto Com Barra",
+            "url_video": "https://www.youtube.com/watch?v=Kr2erpSyu3M",
+            "descricao": "O supino reto com barra permite maior amplitude de movimento, ativando peito, bíceps braquial e tríceps. Requer equilíbrio e coordenação."
+        }
+    ]
+    criar_exercicios_iniciais(db, exercicios)
+    db.close()
+
+# Endpoints da API
+@app.post("/exercicios/", response_model=ExercicioResposta)
+def criar_exercicio_endpoint(exercicio: ExercicioCriar, db: Session = Depends(obter_db)):
+    return criar_exercicio(db, exercicio)
+
+@app.get("/exercicios/{exercicio_id}", response_model=ExercicioResposta)
+def obter_exercicio_endpoint(exercicio_id: int, db: Session = Depends(obter_db)):
+    db_exercicio = obter_exercicio(db, exercicio_id)
+    if db_exercicio is None:
+        raise HTTPException(status_code=404, detail="Exercício não encontrado")
+    return db_exercicio
+
+@app.get("/exercicios/", response_model=list[ExercicioResposta])
+def obter_exercicios_endpoint(skip: int = 0, limit: int = 10, db: Session = Depends(obter_db)):
+    exercicios = obter_exercicios(db, skip=skip, limit=limit)
+    return exercicios
+
+@app.put("/exercicios/{exercicio_id}", response_model=ExercicioResposta)
+def atualizar_exercicio_endpoint(exercicio_id: int, exercicio: ExercicioCriar, db: Session = Depends(obter_db)):
+    db_exercicio = atualizar_exercicio(db, exercicio_id, exercicio)
+    if db_exercicio is None:
+        raise HTTPException(status_code=404, detail="Exercício não encontrado")
+    return db_exercicio
+
+@app.delete("/exercicios/{exercicio_id}", response_model=ExercicioResposta)
+def deletar_exercicio_endpoint(exercicio_id: int, db: Session = Depends(obter_db)):
+    db_exercicio = deletar_exercicio(db, exercicio_id)
+    if db_exercicio is None:
+        raise HTTPException(status_code=404, detail="Exercício não encontrado")
+    return db_exercicio
