d1c925b13d8ac315608d0ea03eb9f795ffdce699
vanessacrmt <142274430+vanessacrmt@users.noreply.github.com>
2024-11-11 20:13:10 -0300

    Update models.py

--- api_gym/models.py
+++ api_gym/models.py
@@ -1,16 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Exercicio(Base):
-    __tablename__ = "exercicios"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
-    url_video = Column(String)
-    descricao = Column(String)
-
 class User(Base):
-    __tablename__ = "users"
+    _tablename_ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
