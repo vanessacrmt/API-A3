bc9c687021bad806ab4a655261b88818651f963e
vanessacrmt <142274430+vanessacrmt@users.noreply.github.com>
2024-11-11 20:15:06 -0300

    Update models.py

--- api_gym/models.py
+++ api_gym/models.py
@@ -1,3 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+class Exercicio(Base):
+    _tablename_ = "exercicios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    url_video = Column(String)
+    descricao = Column(String)
+
 class User(Base):
     _tablename_ = "users"
 
