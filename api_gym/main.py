efc2b0610b856c4ad9f40ba0c09e95d876d7a06e
vanessacrmt <142274430+vanessacrmt@users.noreply.github.com>
2024-11-11 20:10:26 -0300

    Update main.py

--- api_gym/main.py
+++ api_gym/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from contextlib import asynccontextmanager
-from api_gym.auth import criar_token, verificar_token, verificar_credenciais
+from api_gym.auth import criar_token, verificar_credenciais, verificar_token
 from api_gym.database import SessionLocal, engine
 from api_gym.models import Base, User
+from contextlib import asynccontextmanager 
 from api_gym.schemas import ExercicioCriar, ExercicioResposta
 from api_gym.exercicios import (
     criar_exercicios_iniciais,
@@ -31,7 +31,6 @@ def obter_db():
 # Inicialização do banco de dados com exercícios iniciais
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
     db = SessionLocal()
     exercicios = [
           {
@@ -73,14 +72,15 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(obter_db)):
-    user = verificar_credenciais(db, form_data.username, form_data.password)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = verificar_credenciais(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="credenciais incorretas")
-    
-    token = criar_token({"sub": str(user.id)})
-    return {"acesso_token": token, "toke_tipo": "bearer"}
-
+        raise HTTPException(
+            status_code=400, detail="Nome de usuário ou senha incorretos"
+        )
+    token_data = {"sub": user["username"]}
+    token = criar_token(token_data)
+    return {"access_token": token, "token_type": "bearer"}
 
 @app.post("/exercicios/", response_model=ExercicioResposta)
 def criar_exercicio_endpoint(exercicio: ExercicioCriar, db: Session = Depends(obter_db), token: str = Depends(verificar_token)):
